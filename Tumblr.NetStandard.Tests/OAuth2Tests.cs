ecd68c56aa58f6cd234167d2aebdab7c45ce015f
Steven Pears <steven.pears@live.co.uk>
2022-04-03 11:13:01 +0100

    Add OAuth2 support

---
+++ Tumblr.NetStandard.Tests/OAuth2Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tumblr.NetStandard.Tests
+{
+    public class OAuth2Tests
+    {
+        [Fact]
+        public void BuildsAuthUrlCorrect()
+        {
+            var uri = OAuth2.BuildAuthorizeUri("cid", OAuth2.Scopes.OfflineAccess, "state", "re-url");
+            Assert.Equal("https://www.tumblr.com/oauth2/authorize?client_id=cid&response_type=code&scope=offline_access&state=state&redirect_uri=re-url",uri.ToString());
+        }
+
+        [Fact]
+        public void BuildsAuthUrlWithoutRedirectCorrect()
+        {
+            var uri = OAuth2.BuildAuthorizeUri("cid", OAuth2.Scopes.OfflineAccess, "state");
+            Assert.Equal("https://www.tumblr.com/oauth2/authorize?client_id=cid&response_type=code&scope=offline_access&state=state", uri.ToString());
+        }
+
+        [Fact]
+        public void BuildsCodeUrlCorrect()
+        {
+            var uri = OAuth2.BuildAuthTokenUrl("cid", "cis", "12345");
+            Assert.Equal("https://www.tumblr.com/oauth2/token?client_id=cid&client_secret=cis&grant_type=authorization_code&code=12345", uri.ToString());
+        }
+
+        [Fact]
+        public void BuildsRefreshUrlCorrect()
+        {
+            var uri = OAuth2.BuildRefreshTokenUrl("cid", "cis", "12345");
+            Assert.Equal("https://www.tumblr.com/oauth2/token?client_id=cid&client_secret=cis&grant_type=refresh_token&refresh_token=12345", uri.ToString());
+        }
+    }
+}
---
+++ Tumblr.NetStandard/OAuth2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Tumblr.NetStandard
+{
+    public static class OAuth2
+    {
+        public const string AuthorizeUrl = "https://www.tumblr.com/oauth2/authorize";
+        public const string TokenUrl = "https://www.tumblr.com/oauth2/token";
+
+        public static Uri BuildAuthorizeUri(string clientId, string scopes, string state, string redirectUrl = null)
+        {
+            var builder = new UriBuilder(AuthorizeUrl)
+            {
+                Query = $"?client_id={clientId}&response_type=code&scope={scopes}&state={WebUtility.UrlEncode(state)}"
+            };
+
+            if (!string.IsNullOrWhiteSpace(redirectUrl))
+            {
+                builder.Query = builder.Query + "&redirect_uri=" + WebUtility.UrlEncode(redirectUrl);
+            }
+
+            return builder.Uri;
+        }
+
+        public static Uri BuildAuthTokenUrl(string clientId, string clientSecret, string code)
+        {
+            var builder = new UriBuilder(TokenUrl)
+            {
+                Query = $"client_id={clientId}&client_secret={clientSecret}&grant_type=authorization_code&code={code}"
+            };
+
+            return builder.Uri;
+        }
+
+        public static Uri BuildRefreshTokenUrl(string clientId, string clientSecret, string refreshToken)
+        {
+            var builder = new UriBuilder(TokenUrl)
+            {
+                Query = $"client_id={clientId}&client_secret={clientSecret}&grant_type=refresh_token&refresh_token={refreshToken}"
+            };
+
+            return builder.Uri;
+        }
+
+        public static Dictionary<string, string> ParseQuerystring(string query) =>
+            OAuth.Internal.Utility.ParseQueryString(query).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+        public static class Scopes
+        {
+            public const string Basic = "basic";
+            public const string Write = "write";
+            public const string OfflineAccess = "offline_access";
+
+        }
+    }
+}
